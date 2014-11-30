aff32b4c859263bf06af73453cf7490bb2d77429
Walmes Zeviani <walmeszeviani@gmail.com>
2014-11-30 15:27:57 -0200

    Gotham color theme directory added.

---
+++ dotemacs.d/elpa/gotham-theme/gotham-theme-autoloads.el
@@ -0,0 +1,26 @@
+;;; gotham-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "gotham-theme" "gotham-theme.el" (21623 20136
+;;;;;;  247422 444000))
+;;; Generated autoloads from gotham-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("gotham-theme-pkg.el") (21623 20136 458966
+;;;;;;  104000))
+
+;;;***
+
+(provide 'gotham-theme-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; gotham-theme-autoloads.el ends here
